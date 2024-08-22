c203983ec3b35dd3a2ca84b05f89bd630dff6681
Nikita-Barbos <sobemov@gmail.com>
2024-08-23 00:42:40 +0300

    Вступление

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        var box = 5;
+        System.out.println(box);
+        var capacityLeft = 30;
+        System.out.println("Можно положить " + capacityLeft + " кг вещей");
+
+
+        var dog = 8.0;
+        System.out.println(dog);
+        var cat = 3.6;
+        System.out.println(cat);
+        var paper = 763789;
+        System.out.println(paper);
+
+    }
+}
